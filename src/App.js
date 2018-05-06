b1e142f9d917558ddad4b3261751aebcc415c519
Valerio Innocenti Sedili <valerio.innocenti.ext@yoox.com>
2018-05-06 09:45:02 +0200

    add bulma

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className={styles.intro}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <input className={styles.input} type="text" placeholder="Info input" />
       </div>
     );
   }
